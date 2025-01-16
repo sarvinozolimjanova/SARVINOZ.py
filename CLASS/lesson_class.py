7aaa32a1427d38603e2e6b83dade29663c025ead
sarvinozolimjanova <sarvinozolimjonova073@gmail.com>
2025-01-16 10:56:55 +0500

    changes

--- CLASS/lesson_class.py
+++ CLASS/lesson_class.py
@@ -24,9 +24,9 @@ car1 = Car("BMW", "m8", "black", 300_000, 450, 2012)
 car2 = Car("Mercedes-Benz", 'cls63', 'black', 300_000, 300, 2020)
 car3 = Car('Bentley', "i don't know", 'white', 500_000, 300, 2009)
 
-print(car1.get_info())
-print(car2.get_info())
-print(car3.get_info())
+# print(car1.get_info())
+# print(car2.get_info())
+# print(car3.get_info())
 
 
 
@@ -35,3 +35,22 @@ print(car3.get_info())
 # git config --global user.email "youremail@domain.com"
 
 
+""" 1 - mashq """
+class Student():
+    """ """
+    
+    def __init__(self, name:str, surname:str, kurs:int, marks:list):
+        self.name = name
+        self.surname = surname
+        self.kurs = kurs
+        self.marks = marks
+
+    def get_info(self):
+        return sum(self.marks)/len(self.marks)
+
+
+durbek = Student("Durbek", "Muhiddinov", 2, [3,4,4,3])
+ali = Student("Ali", "Bahodirov", 4, [5, 4, 4, 3])
+
+print(durbek.get_info())
+print(ali.get_info())
